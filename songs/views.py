b5dd08ad6aefe0af927c7d274e71db7ba3b4f45c
Ignacio Feijoo <ignacionf@gmail.com>
2022-11-03 01:04:34 -0300

    search, query top50 and delete songs

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import status
 
-# Create your views here.
+from .models import Song
+from .serializers import SongSerializer
+
+
+class SongViewSet(viewsets.ViewSet):
+    @action(detail=False, methods=["get"])
+    def search(self, request):
+
+        q = request.GET.get("q", None)
+
+        if q:
+            q = f"%{q}%"
+            query = 'SELECT * FROM songs_song as s INNER JOIN artists_artist ON (s.artist_id = artists_artist.id) WHERE (s.name LIKE %s OR artists_artist.name LIKE %s) ORDER BY s."order" ASC'
+            qs = Song.objects.raw(query, [q, q])
+        else:
+            query = "SELECT * FROM songs_song as s INNER JOIN artists_artist ON (s.artist_id = artists_artist.id)"
+            qs = Song.objects.raw(query)
+
+        return Response(SongSerializer(qs, many=True).data)
+
+    @action(detail=False, methods=["get"])
+    def top50(self, request):
+
+        query = 'SELECT * FROM songs_song as s INNER JOIN artists_artist ON (s.artist_id = artists_artist.id) WHERE (s."order" <=50) ORDER BY s."order" ASC '
+        qs = Song.objects.raw(query)
+
+        return Response(SongSerializer(qs, many=True).data)
+
+    def destroy(self, request, pk):
+
+        try:
+            Song.objects.get(pk=int(pk)).delete()
+        except (Song.DoesNotExist, ValueError):
+            return Response(
+                {"errors": ["Song invalid"]}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        return Response({"action": "deleted"})
+
+    def create(self, request):
+        Song.create(*request.POST)
+        return Response({"action": "created"})
